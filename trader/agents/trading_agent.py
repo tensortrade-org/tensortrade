8f0cbe55ddc9ba81500cfc464a82da6f8867f978
adamjking3 <adamjking3@gmail.com>
2019-07-29 00:06:17 -0700

    Add TradingAgent abstract class

--- trader/agents/trading_agent.py
+++ trader/agents/trading_agent.py
@@ -1,6 +1,8 @@
 from abc import ABCMeta, abstractmethod
+from typing import Callable, List
 
 from trader.features import FeaturePipeline
+from trader.performance import TradingPerformance
 
 
 class TradingAgent(object, metaclass=ABCMeta):
@@ -8,5 +10,18 @@ class TradingAgent(object, metaclass=ABCMeta):
     def __init__(self):
         pass
 
+    @abstractmethod
+    def train(self, steps: int, callback: Callable[[TradingPerformance], [bool]]) -> TradingPerformance:
+        pass
+
+    @abstractmethod
+    def evaluate(self, steps: int, callback: Callable[[TradingPerformance], [bool]]) -> TradingPerformance:
+        pass
+
+    @abstractmethod
+    def predict(self, observation: List) -> int | tuple:
+        pass
+
+    @abstractmethod
     def add_feature_pipeline(self, pipeline: FeaturePipeline):
         pass
