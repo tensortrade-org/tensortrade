2e2ba7e502aaa685d4ed5f1576a0b5c6059645eb
Adam King <adamjking3@gmail.com>
2019-08-18 10:44:31 -0700

    Create initial rl models

--- tensortrade/models/rl/dqn.py
+++ tensortrade/models/rl/dqn.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License
+#
+# Original paper: https://www.cs.toronto.edu/~vmnih/docs/dqn.pdf
 
 import tensorflow as tf
 
