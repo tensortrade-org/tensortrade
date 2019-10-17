d7ac59cc791d6d63714d84f969f93f750dad562c
Ryan <rshtirmer@gmail.com>
2019-10-17 22:34:09 +0000

    remove unnecessary code

--- tensortrade/rewards/__init__.py
+++ tensortrade/rewards/__init__.py
@@ -1,6 +1,5 @@
 from .reward_strategy import RewardStrategy
 from .simple_profit_strategy import SimpleProfitStrategy
-from .running_profit_strategy import RunningProfitStrategy
 
 _registry = {
     'simple': SimpleProfitStrategy(),
