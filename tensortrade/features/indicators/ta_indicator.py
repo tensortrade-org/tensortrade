b9943e6c6f12f69cb494994c6eba078a12a45f07
daHIFI <dahifi@gmail.com>
2019-10-21 22:11:04 -0400

    indicators added, working on tests

---
+++ tensortrade/features/indicators/ta_indicator.py
@@ -0,0 +1,124 @@
+# Copyright 2019 The TensorTrade Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import ta
+import numpy as np
+import pandas as pd
+
+from gym import Space
+from copy import copy
+from abc import abstractmethod
+from typing import Union, List, Callable
+
+from tensortrade.features.feature_transformer import FeatureTransformer
+
+
+class TAIndicator(FeatureTransformer):
+    """Adds one or more TA indicators to a data frame, based on existing open, high, low, and close column values."""
+    indicators = [
+        # Momentum
+        ('AO', ta.ao, ['High', 'Close']),
+        ('KAMA', ta.kama, ['Close']),
+        ('MFI', ta.money_flow_index, ['High', 'Low', 'Close', 'VolumeFrom']),
+        ('RSI', ta.rsi, ['Close']),
+        ('SO', ta.stoch, ['Close']),
+        ('SOS', ta.stoch_signal, ['High','Low', 'Close']),
+        ('TSI', ta.tsi, ['Close']),
+        ('UO', ta.uo, ['High', 'Low', 'Close']),
+        ('WR', ta.wr, ['High', 'Low', 'Close']),
+        # Volume
+        ('ADI', ta.acc_dist_index, ['High', 'Low', 'Close', 'VolumeFrom']),
+        ('CMF', ta.chaikin_money_flow, ['High', 'Low', 'Close', 'VolumeFrom']),
+        ('EM', ta.ease_of_movement, ['High', 'Low', 'Close', 'VolumeFrom']),
+        ('FI', ta.force_index, ['Close', 'Volume BTC']),
+        ('NVI', ta.negative_volume_index, ['Close', 'Volume BTC']),
+        ('OBV', ta.on_balance_volume, ['Close', 'Volume BTC']),
+        ('PCR', ta.put_call_ratio, ['Close', 'Volume BTC']),
+        ('VPT', ta.volume_price_trend, ['Close', 'Volume BTC']),
+        # Volatility
+        ('ATR', ta.average_true_range, ['High', 'Low', 'Close']),
+        ('BBH', ta.bollinger_hband, ['Close']),
+        ('BBHI', ta.bollinger_hband_indicator, ['Close']),
+        ('BBL', ta.bollinger_lband, ['Close']),
+        ('BBLI', ta.bollinger_lband_indicator, ['Close']),
+        ('BBM', ta.bollinger_mavg, ['Close']),
+        ('DCH', ta.donchian_channel_hband, ['Close']),
+        ('DCHI', ta.donchian_channel_hband_indicator, ['Close']),
+        ('DCL', ta.donchian_channel_lband, ['Close']),
+        ('DCLI', ta.donchian_channel_lband_indicator, ['Close']),
+        ('KCC', ta.keltner_channel_central, ['High', 'Low', 'Close']),
+        ('KCH', ta.keltner_channel_hband, ['High', 'Low', 'Close']),
+        ('KCHI', ta.keltner_channel_hband_indicator, ['High', 'Low', 'Close']),
+        ('KCL', ta.keltner_channel_lband, ['High', 'Low', 'Close']),
+        ('KCLI', ta.keltner_channel_lband_indicator, ['High', 'Low', 'Close']),
+        # Trend
+        ('AXD', ta.adx, ['High', 'Low', 'Close']),
+        ('AXDN', ta.adx_neg, ['High', 'Low', 'Close']),
+        ('AXDP', ta.adx_pos, ['High', 'Low', 'Close']),
+        ('ARD', ta.aroon_down, ['Close']),
+        ('ARU', ta.aroon_up, ['Close']),
+        ('CCI', ta.cci, ['High', 'Low', 'Close']),
+        ('DPO', ta.dpo, ['Close']),
+        ('EMA', ta.trend.ema_indicator, ['Close']),
+        ('ICHA', ichimoku_a, ['High', 'Low']),
+        ('ICHB', ichimoku_b, ['High', 'Low']),
+        ('KST', ta.kst, ['Close']),
+        ('KSTS', ta.kst_sig, ['Close']),
+        ('MACD', ta.macd, ['Close']),
+        ('MACDD', ta.macd_diff, ['Close']),
+        ('MACDS', ta.macd_signal, ['Close']),
+        ('MI', ta.mass_index, ['High', 'Low']),
+        ('TRIX', ta.trix, ['Close']),
+        ('VIN', ta.vortex_indicator_neg, ['High', 'Low', 'Close']),
+        ('VIP', ta.vortex_indicator_pos, ['High', 'Low', 'Close']),
+        # Others
+        ('CR', ta.cumulative_return, ['Close']),
+        ('DLR', ta.daily_log_return, ['Close']),
+        ('DR', ta.daily_return, ['Close']),
+
+    ]
+
+
+
+    def __init__(self, indicators: List[str], lows: Union[List[float], List[int]] = None, highs: Union[List[float], List[int]] = None):
+        self._indicator_names = indicators
+        self._indicators = list(
+            map(lambda indicator_name: self._str_to_indicator(indicator_name), indicators))
+
+        self._lows = lows or np.zeros(len(indicators))
+        self._highs = highs or np.ones(len(indicators))
+
+    def _str_to_indicator(self, indicator_name: str):
+        return getattr(ta, indicator_name.upper())
+
+    def transform_space(self, input_space: Space, column_names: List[str]) -> Space:
+        output_space = copy(input_space)
+        shape_x, *shape_y = input_space.shape
+
+        output_space.shape = (shape_x + len(self._indicators), *shape_y)
+
+        for i in range(len(self._indicators)):
+            output_space.low = np.append(output_space.low, self._lows[i])
+            output_space.high = np.append(output_space.high, self._highs[i])
+
+        return output_space
+
+    def transform(self, X: pd.DataFrame, input_space: Space) -> pd.DataFrame:
+        for i in range(len(self._indicators)):
+            indicator_name = self._indicator_names[i]
+            indicator = self._indicators[i]
+
+            X[indicator_name.upper()] = indicator(X['open'], X['high'], X['low'], X['close'])
+
+        return X
---
+++ tests/tensortrade/features/indicators/test_ta_indicator.py
@@ -0,0 +1,104 @@
+import pytest
+import numpy as np
+import pandas as pd
+import tensortrade.exchanges as exchanges
+
+import ta
+
+from gym.spaces import Box
+
+from tensortrade.features.indicators import ta_indicator
+from tensortrade.features.stationarity import FractionalDifference
+
+
+@pytest.fixture
+def exchange():
+    return exchanges.get('fbm')
+
+
+@pytest.fixture
+def data_frame():
+    df = pd.read_csv('./tensortrade/data/input/coinbase-1h-btc-usd.csv')
+    return df
+
+
+class TestTAIndicator():
+    def test_ta_indicator(self):
+        df = data_frame()
+        df = ta.add_all_ta_features(df, 'Open', 'High', 'Low', 'Close', 'VolumeFrom')
+
+
+
+    def test_transform_space(self):
+        pass
+
+    def test_transform(self):
+        pass
+
+    def test_incremental_transform(self, data_frame, exchange):
+        difference_all = FractionalDifference(
+            difference_order=0.5, inplace=True)
+
+        feature_pipeline = FeaturePipeline(steps=[difference_all])
+
+        transformed_frame = feature_pipeline.transform(data_frame, exchange.generated_space)
+
+        expected_data_frame = pd.DataFrame([{
+            'open': -26.20469322,
+            'low': -46.15180724,
+            'high': 33.63664884,
+            'close': 13.68953482,
+        },
+            {
+            'open': 134.53651465,
+            'low': 118.24976426,
+            'high': 183.39676584,
+            'close': 167.11001545,
+        }])
+
+        assert np.allclose(expected_data_frame.values, transformed_frame.values)
+
+        next_frame = pd.DataFrame([{
+            'open': 200,
+            'low': 150,
+            'high': 350,
+            'close': 300,
+        },
+            {
+            'open': 300,
+            'low': 250,
+            'high': 450,
+            'close': 400,
+        }])
+
+        transformed_frame = feature_pipeline.transform(next_frame, exchange.generated_space)
+
+        expected_data_frame = pd.DataFrame([{
+            'open': 127.785105,
+            'low': 87.031409,
+            'high': 250.046192,
+            'close': 209.292496,
+        },
+            {
+            'open': 185.484853,
+            'low': 166.817514,
+            'high': 241.486873,
+            'close': 222.819533,
+        }])
+
+        assert np.allclose(expected_data_frame.values, transformed_frame.values)
+
+    def test_transform_space(self, data_frame, exchange):
+        difference_all = FractionalDifference(difference_order=0.5, inplace=False)
+
+        feature_pipeline = FeaturePipeline(steps=[difference_all])
+
+        low = np.array([1E-3, ] * 4 + [1E-3, ])
+        high = np.array([1E3, ] * 4 + [1E3, ])
+
+        input_space = Box(low=low, high=high, dtype=np.float16)
+
+        transformed_space = feature_pipeline.transform_space(
+            input_space, exchange.generated_columns)
+
+        assert transformed_space != input_space
