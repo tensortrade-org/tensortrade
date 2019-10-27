490a7d2f528c3f954c8ffaf9d86b5806245132c8
Bradley Watson <bwats127@gmail.com>
2019-10-27 13:10:56 +1000

    Update __init__.py
    Added the new TargetStopActionStrategy

--- tensortrade/actions/__init__.py
+++ tensortrade/actions/__init__.py
@@ -2,12 +2,14 @@ from .action_strategy import ActionStrategy, DTypeString, TradeActionUnion
 from .continuous_action_strategy import ContinuousActionStrategy
 from .discrete_action_strategy import DiscreteActionStrategy
 from .multi_discrete_action_strategy import MultiDiscreteActionStrategy
+from .target_stop_action_strategy import TargetStopActionStrategy
 
 
 _registry = {
     'continuous': ContinuousActionStrategy(),
     'discrete': DiscreteActionStrategy(),
     'multi-discrete': MultiDiscreteActionStrategy(instrument_symbols=['BTC', 'ETH']),
+    'target-stop': TargetStopActionStrategy,
 }
 
 
