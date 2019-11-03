8113c5b3a2cc21afb7df33855ab1b8918a25cf54
daHIFI <dahifi@gmail.com>
2019-11-02 22:55:34 -0400

    refactored to call ta functions directly.

--- tensortrade/features/indicators/ta_indicator.py
+++ tensortrade/features/indicators/ta_indicator.py
@@ -29,88 +29,24 @@ class TAIndicator(FeatureTransformer):
      column values.."""
 
     # Indicators supported by TA module:
-    indicators = [
-        # Momentum
-        ('AO', ta.ao, ['High', 'Close']),
-        #('KAMA', ta.kama, ['Close']),
-        ('MFI', ta.money_flow_index, ['High', 'Low', 'Close', 'VolumeFrom']),
-        ('RSI', ta.rsi, ['Close']),
-        ('SO', ta.stoch, ['Close']),
-        ('SOS', ta.stoch_signal, ['High','Low', 'Close']),
-        ('TSI', ta.tsi, ['Close']),
-        ('UO', ta.uo, ['High', 'Low', 'Close']),
-        ('WR', ta.wr, ['High', 'Low', 'Close']),
-        # Volume
-        ('ADI', ta.acc_dist_index, ['High', 'Low', 'Close', 'VolumeFrom']),
-        ('CMF', ta.chaikin_money_flow, ['High', 'Low', 'Close', 'VolumeFrom']),
-        ('EM', ta.ease_of_movement, ['High', 'Low', 'Close', 'VolumeFrom']),
-        ('FI', ta.force_index, ['Close', 'Volume BTC']),
-        ('NVI', ta.negative_volume_index, ['Close', 'Volume BTC']),
-        ('OBV', ta.on_balance_volume, ['Close', 'Volume BTC']),
-        ('PCR', ta.put_call_ratio, ['Close', 'Volume BTC']),
-        ('VPT', ta.volume_price_trend, ['Close', 'Volume BTC']),
-        # Volatility
-        ('ATR', ta.average_true_range, ['High', 'Low', 'Close']),
-        ('BBH', ta.bollinger_hband, ['Close']),
-        ('BBHI', ta.bollinger_hband_indicator, ['Close']),
-        ('BBL', ta.bollinger_lband, ['Close']),
-        ('BBLI', ta.bollinger_lband_indicator, ['Close']),
-        ('BBM', ta.bollinger_mavg, ['Close']),
-        ('DCH', ta.donchian_channel_hband, ['Close']),
-        ('DCHI', ta.donchian_channel_hband_indicator, ['Close']),
-        ('DCL', ta.donchian_channel_lband, ['Close']),
-        ('DCLI', ta.donchian_channel_lband_indicator, ['Close']),
-        ('KCC', ta.keltner_channel_central, ['High', 'Low', 'Close']),
-        ('KCH', ta.keltner_channel_hband, ['High', 'Low', 'Close']),
-        ('KCHI', ta.keltner_channel_hband_indicator, ['High', 'Low', 'Close']),
-        ('KCL', ta.keltner_channel_lband, ['High', 'Low', 'Close']),
-        ('KCLI', ta.keltner_channel_lband_indicator, ['High', 'Low', 'Close']),
-        # Trend
-        ('AXD', ta.adx, ['High', 'Low', 'Close']),
-        ('AXDN', ta.adx_neg, ['High', 'Low', 'Close']),
-        ('AXDP', ta.adx_pos, ['High', 'Low', 'Close']),
-        ('ARD', ta.aroon_down, ['Close']),
-        ('ARU', ta.aroon_up, ['Close']),
-        ('CCI', ta.cci, ['High', 'Low', 'Close']),
-        ('DPO', ta.dpo, ['Close']),
-        ('EMA', ta.trend.ema_indicator, ['Close']),
-        ('ICHA', ta.ichimoku_a, ['High', 'Low']),
-        ('ICHB', ta.ichimoku_b, ['High', 'Low']),
-        ('KST', ta.kst, ['Close']),
-        ('KSTS', ta.kst_sig, ['Close']),
-        ('MACD', ta.macd, ['Close']),
-        ('MACDD', ta.macd_diff, ['Close']),
-        ('MACDS', ta.macd_signal, ['Close']),
-        ('MI', ta.mass_index, ['High', 'Low']),
-        ('TRIX', ta.trix, ['Close']),
-        ('VIN', ta.vortex_indicator_neg, ['High', 'Low', 'Close']),
-        ('VIP', ta.vortex_indicator_pos, ['High', 'Low', 'Close']),
-        # Others
-        ('CR', ta.cumulative_return, ['Close']),
-        ('DLR', ta.daily_log_return, ['Close']),
-        ('DR', ta.daily_return, ['Close']),
-
-    ]
 
     def __init__(self,
                  indicators: Union[List[str], str, None] = None,
                  lows: Union[List[float], List[int]] = None,
                  highs: Union[List[float], List[int]] = None
                  ):
-        """S
-        :param columns (optional): A list of indicators to apply as part of the transformation.  Leaving this value
-        blank will apply all that are supported by the library.
-        """
 
-        self._indicator_names = TAIndicator.indicators if not indicators else indicators
+        if isinstance(indicators, str):
+            indicators = [indicators]
+
+        self._indicator_names = indicators
         self._indicators = list(
             map(lambda indicator_name: self._str_to_indicator(indicator_name), indicators))
         self._lows = lows or np.zeros(len(indicators))
         self._highs = highs or np.ones(len(indicators))
 
     def _str_to_indicator(self, indicator_name: str):
-        # Only one result expected
-        return next(i for i in TAIndicator.indicators if i[0] == indicator_name)
+        return getattr(ta, indicator_name.lower())
 
     def transform_space(self, input_space: Space, column_names: List[str]) -> Space:
         output_space = copy(input_space)
@@ -129,15 +65,23 @@ class TAIndicator(FeatureTransformer):
         Will add TAIndicator.indicator columns to DataFrame. Frame must have columns that match indicator parameters,
         e.g. ['High', 'Low', 'Close'], &c...
 
-        :param df: Dataframe with columns matching TA indicators function call
-        :param input_space: None is acceptable, doesn't nothing but required by abstract class
+        :param df: Dataframe with columns matching TA indicators function call. Not case sensitive
+        :param input_space: None is acceptable, does nothing but required by abstract class
         :return:
         """
         for i in range(len(self._indicators)):
             indicator = self._indicators[i]
-            name, func, args = indicator
-            split = [df[a] for a in args]
-            df[name] = func(*split)
+            indicator_name = self._indicator_names[i]
+            params = {}
+            for param in indicator.__code__.co_varnames:
+                if param in ["df", "open", "high", "low", "close", "volume"]:
+                    if param == "df":
+                        params[param] = df
+                    else:
+                        for column in df.columns:
+                            if column.lower() == param:
+                                params[param] = df[column]
+            df[indicator_name] = indicator(**params)
         return df
 
 
--- tests/tensortrade/features/indicators/test_ta_indicator.py
+++ tests/tensortrade/features/indicators/test_ta_indicator.py
@@ -1,13 +1,7 @@
 import pytest
-import os
 import numpy as np
 import pandas as pd
-import tensortrade.exchanges as exchanges
-
-import ta
-
 from gym.spaces import Box
-
 from tensortrade.features.indicators.ta_indicator import TAIndicator
 
 
@@ -23,18 +17,16 @@ def input_space():
 
 
 class TestTAIndicator:
-    indicators_to_test = ["AO", "MFI", "RSI"]
+    indicators_to_test = ['rsi', 'macd', 'ema_indicator']
 
     def test_ta_indicator(self):
         test_feature = TAIndicator(TestTAIndicator.indicators_to_test)
         assert len(test_feature._indicator_names) == 3
-        assert ('AO', ta.ao, ['High', 'Close']) in test_feature.indicators
 
     def test_transform_space(self, input_space, data_frame):
         test_feature = TAIndicator(TestTAIndicator.indicators_to_test)
         column_names = data_frame.columns
         output = test_feature.transform_space(input_space, column_names)
-
         assert output.shape[0] > input_space.shape[0]
         assert output.shape[1] == input_space.shape[1]
 
@@ -42,7 +34,20 @@ class TestTAIndicator:
         test_feature = TAIndicator(TestTAIndicator.indicators_to_test)
         test_feature.transform(data_frame, None)
         assert set(TestTAIndicator.indicators_to_test).issubset(data_frame.columns)
+        print(data_frame.rsi)
 
+    def test_transform_single_indicator(self, data_frame):
+        test_feature = TAIndicator('rsi')
+        test_feature.transform(data_frame, None)
+        assert 'rsi' in data_frame.columns
+        print(data_frame.rsi)
+
+    def test_add_volatility_ta(self, data_frame):
+        test_feature = TAIndicator('add_volatility_ta')
+        print(test_feature._indicator_names)
+        test_feature.transform(data_frame, None)
+        print(data_frame.columns)
+        print(data_frame.dtypes)
 
 
 
