1571945cb149a36abedb40944ddce13189c03433
funlyonions <admin@example.com>
2019-11-28 16:46:29 -0600

    Added useless test in random slippage

---
+++ tests/tensortrade/slippage/test_random_slippage_model.py
@@ -0,0 +1,2 @@
+def test_slippage_in_bounds():
+    assert 1 < 2 < 3
--- tests/tensortrade/slippage/test_slippage_model.py
+++ tests/tensortrade/slippage/test_slippage_model.py
@@ -2,3 +2,5 @@
 from tensortrade import TradingContext
 from tensortrade.slippage import SlippageModel
 from tensortrade.trades import Trade
+
+
