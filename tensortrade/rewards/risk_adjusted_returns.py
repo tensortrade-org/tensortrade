Fixing wrong sortino reward formula
hey! so I found a bug in the sortino formula.. firstly it should be downside_returns[..] = .. on line 59 and downside_returns.std() instead of downside_returns.mean() on line 62 since its a deviation and not a mean.

tensortrade/rewards/risk_adjusted_returns.py
@@ -56,10 +56,10 @@ class RiskAdjustedReturns(RewardScheme):
         """
         downside_returns = pd.Series([0])
 
-        returns[returns < self._target_returns] = returns ** 2
+        downside_returns[returns < self._target_returns] = returns ** 2
 
         expected_return = returns.mean()
-        downside_std = np.sqrt(downside_returns.mean())
+        downside_std = np.sqrt(downside_returns.std())
 
         return (expected_return - self._risk_free_rate) / (downside_std + 1E-9)
 