b65118e32e85ed7f9a839faa9b84739a32a8a7d4
Miguel Angel G <miguel@gordian.dev>
2019-12-08 04:34:16 -0600

    Add testing for reward scheme and simple profit reward

--- tests/tensortrade/rewards/test_reward_scheme.py
+++ tests/tensortrade/rewards/test_reward_scheme.py
@@ -1,4 +1,29 @@
 
 from tensortrade import TradingContext
+from tensortrade.exchanges import Exchange
 from tensortrade.rewards import RewardScheme
-from tensortrade.trades import Trade
+
+config = {
+    'base_instrument': 'EURO',
+    'instruments': 'ETH',
+    'exchanges': {
+        'credentials': {
+            'api_key': '48hg34wydghi7ef',
+            'api_secret_key': '0984hgoe8d7htg'
+        }
+    }
+}
+
+
+def test_exchange_setup():
+    with TradingContext(**config):
+        reward_scheme = RewardScheme()
+        exchange = Exchange(0.0)
+        reward_scheme.exchange = exchange
+
+        assert reward_scheme.exchange is exchange
+
+
+def test_reset_state_scheme():
+    reward_scheme = RewardScheme()
+    assert reward_scheme.reset() is None
--- tests/tensortrade/rewards/test_simple_profit.py
+++ tests/tensortrade/rewards/test_simple_profit.py
@@ -1,3 +1,40 @@
+from unittest import mock
 
-from tensortrade import TradingContext
+import numpy as np
+
 from tensortrade.rewards import SimpleProfit
+
+
+def test_reward_not_holding_a_position():
+    reward = SimpleProfit()
+    trade = mock.Mock()
+    trade.configure_mock(is_hold=False, is_buy=False, is_sell=False, amount=0, price=0)
+    assert reward.get_reward(0, trade) == -1
+
+    # TODO: Implement all cases for not holding
+
+def test_reward_holding_a_position():
+    reward = SimpleProfit()
+    trade = mock.Mock()
+    trade.configure_mock(is_hold=True, is_buy=False, is_sell=False, amount=0, price=0)
+    assert reward.get_reward(0, trade) == 1
+
+def test_reward_opening_a_position():
+    reward = SimpleProfit()
+    trade = mock.Mock()
+    trade.configure_mock(is_hold=False, is_buy=True, is_sell=False, amount=1000, price=100)
+    assert reward.get_reward(0, trade) == 2
+
+def test_reward_closing_a_position():
+    reward = SimpleProfit()
+    trade = mock.Mock()
+    # Setup purchase price and hold instrument
+    trade.configure_mock(is_hold=False, is_buy=True, is_sell=False, amount=1000, price=100)
+    reward.get_reward(0, trade)
+
+    trade.configure_mock(is_hold=False, is_buy=False, is_sell=True, amount=1000, price=1000)
+    profit_per_instrument = 1000 - 100
+    profit = 1000 * profit_per_instrument
+    profit_sign = np.sign(profit)
+
+    assert reward.get_reward(0, trade) == profit_sign * (1 + (5 ** np.log10(abs(profit))))
