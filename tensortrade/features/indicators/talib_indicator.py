bc90c286d42e27e5c40a70ec653f4b8f9e6c6e73
Raed Shabbir <raed@knockri-ai.y1jrskmyuj0uhb3rinxkk4i1yc.ex.internal.cloudapp.net>
2020-01-17 17:17:44 +0000

    fixed talib history

--- tensortrade/features/indicators/talib_indicator.py
+++ tensortrade/features/indicators/talib_indicator.py
@@ -25,32 +25,57 @@ class TAlibIndicator(FeatureTransformer):
 
     def __init__(self, indicators: List[str], lows: Union[List[float], List[int]] = None, highs: Union[List[float], List[int]] = None, **kwargs):
         self._indicator_names = [indicator[0].upper() for indicator in indicators]
+        self._indicators = [getattr(talib, name.split('-')[0]) for name in self._indicator_names]
         self._indicator_args = {indicator[0]:indicator[1]['args'] for indicator in indicators}
         self._indicator_params = {indicator[0]: indicator[1]['params'] for indicator in indicators}
-        self._indicators = [getattr(talib, name.split('-')[0]) for name in self._indicator_names]
+
+        #columns 
+        self._ohlcv_cols = ['date','open', 'high', 'low', 'close', 'volume', 'volumeto']
+        self._other_cols = [indicator_name.upper() for indicator_name in self._indicator_names if indicator_name != "BBANDS"]
+        self._bband_cols = ["bb_upper", "bb_middle", "bb_lower"]
+
+    def init_history(self):
+        self._history = pd.DataFrame(columns = self._ohlcv_cols + self._other_cols + self._bband_cols)
+
+    def observation_columns(self):
+        return self._other_cols+self._bband_cols
+
+    def transform_spaces(self, low, high):
+        new_low, new_high = low.copy(), high.copy()
+        return new_low, new_high
 
     def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        #concat new observations to the history 
+        window_size = len(X)
+        self._history = pd.concat([self._history, X], ignore_index=False)
+
         for idx, indicator in enumerate(self._indicators):
             indicator_name = self._indicator_names[idx]
-            indicator_args = [X[arg].values for arg in self._indicator_args[indicator_name]]
+            indicator_args = [self._history[arg].values for arg in self._indicator_args[indicator_name]]
             indicator_params = self._indicator_params[indicator_name]
 
             if indicator_name == 'BBANDS':
                 upper, middle, lower = indicator(*indicator_args,**indicator_params)
 
-                X["bb_upper"] = upper
-                X["bb_middle"] = middle
-                X["bb_lower"] = lower
+                self._history["bb_upper"] = upper
+                self._history["bb_middle"] = middle
+                self._history["bb_lower"] = lower
             else:
                 try:
                     value = indicator(*indicator_args,**indicator_params)
 
                     if type(value) == tuple:
-                        X[indicator_name] = value[0][0]
+                        self._history[indicator_name] = value[0][0]
                     else:
-                        X[indicator_name] = value
+                        self._history[indicator_name] = value
 
                 except:
-                    X[indicator_name] = indicator(*indicator_args,**indicator_params)[0]
+                    self._history[indicator_name] = indicator(*indicator_args,**indicator_params)[0]
 
-        return X
+        #return window size of observations 
+        return self._history.tail(window_size)
+    
+    def reset(self):
+        self.init_history()
+        super().reset()
+
