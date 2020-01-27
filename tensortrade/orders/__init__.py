ef0ef95f2576a32e33dc0ce4e35b539cc81864ce
Adam King <adamjking3@gmail.com>
2020-01-26 20:02:45 -0800

    Fix import order.

--- tensortrade/orders/__init__.py
+++ tensortrade/orders/__init__.py
@@ -1,8 +1,8 @@
+from .trade import Trade, TradeSide, TradeType
 from .broker import Broker
 from .order import Order, OrderStatus
 from .order_listener import OrderListener
 from .order_spec import OrderSpec
-from .trade import Trade, TradeSide, TradeType
 
 from .create import market_order, limit_order, hidden_limit_order, risk_managed_order
 
