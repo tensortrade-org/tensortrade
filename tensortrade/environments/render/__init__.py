eeeffe9188d0a9f6113e0967c9e6ca4d74df24b3
Yasser Hassan <yhassan@yahoo.com>
2020-02-10 17:50:58 +0200

    Update render registry.

--- tensortrade/environments/render/__init__.py
+++ tensortrade/environments/render/__init__.py
@@ -1,10 +1,16 @@
 import importlib
 
+
+_registry = {}
+
+
 if importlib.util.find_spec("matplotlib") is not None:
     from .matplotlib_trading_chart import MatplotlibTradingChart
-from .plotly_stock_chart import PlotlyTradingChart
 
-_registry = {
-    'matplotlib': MatplotlibTradingChart,
-    'plotly': PlotlyTradingChart
-}
+    _registry['matplotlib'] = MatplotlibTradingChart
+
+
+if importlib.util.find_spec("plotly") is not None:
+    from .plotly_stock_chart import PlotlyTradingChart
+
+    _registry['plotly'] = PlotlyTradingChart
