83f3fbfb795614f96b708acd8d0c926882b0bb23
Yasser Hassan <yhassan@yahoo.com>
2020-02-12 00:35:59 +0200

    Add missing section and code cleanup.

--- tensortrade/agents/dqn_agent.py
+++ tensortrade/agents/dqn_agent.py
@@ -1,15 +1,30 @@
+# Copyright 2019 The TensorTrade Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+
+
 """
 References:
     - https://towardsdatascience.com/deep-reinforcement-learning-build-a-deep-q-network-dqn-to-play-cartpole-with-tensorflow-2-and-gym-8e105744b998
     - https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html#dqn-algorithm
 """
 
-import uuid
+
 import random
 import numpy as np
 import tensorflow as tf
 
-from typing import Callable, Tuple
+from typing import Callable
 from collections import namedtuple
 
 from tensortrade.agents import Agent, ReplayMemory
