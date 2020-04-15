c2a51092826c652e95f74b2cd2f5f9b3638eb603
kodiakcrypto <62376262+kodiakcrypto@users.noreply.github.com>
2020-04-14 22:25:22 -0700

    Making the Class work
    Removed the self/init structure to make it more noob friendly to use

--- tensortrade/utils/cryptodatadownload.py
+++ tensortrade/utils/cryptodatadownload.py
@@ -1,29 +1,39 @@
-
 import pandas as pd
+import ssl # Used if pandas gives a SSLError
+ssl._create_default_https_context = ssl._create_unverified_context
 
 
 class CryptoDataDownload:
 
-    def __init__(self):
-        self.url = "https://www.cryptodatadownload.com/cdd/"
+    url = "https://www.cryptodatadownload.com/cdd/"
 
-    def fetch_default(self, exchange_name, base_symbol, quote_symbol, timeframe, include_all_volumes=False):
-
-        filename = "{}_{}{}_{}.csv".format(exchange_name, quote_symbol, base_symbol, timeframe)
+    def fetch_default(exchange_name, 
+                      base_symbol, 
+                      quote_symbol, 
+                      timeframe, 
+                      include_all_volumes=False):
+        
+        filename = "{}_{}{}_{}.csv".format(exchange_name, 
+                                           base_symbol, 
+                                           quote_symbol, 
+                                           timeframe)
         base_vc = "Volume {}".format(base_symbol)
         new_base_vc = "volume_base"
         quote_vc = "Volume {}".format(quote_symbol)
         new_quote_vc = "volume_quote"
 
-        df = pd.read_csv(self.url + filename, skiprows=1)
+        df = pd.read_csv(CryptoDataDownload.url + filename, skiprows=1)
         df = df[::-1]
         df = df.drop(["Symbol"], axis=1)
-        df = df.rename({base_vc: new_base_vc, quote_vc: new_quote_vc, "Date": "date"}, axis=1)
+        df = df.rename({base_vc: new_base_vc, 
+                        quote_vc: new_quote_vc, 
+                        "Date": "date"}, axis=1)
 
         if "d" in timeframe:
             df["date"] = pd.to_datetime(df["date"])
         elif "h" in timeframe:
-            df["date"] = pd.to_datetime(df["date"], format="%Y-%m-%d %I-%p")
+            df["date"] = pd.to_datetime(df["date"], 
+                                        format="%Y-%m-%d %I-%p")
 
         df = df.set_index("date")
         df.columns = [name.lower() for name in df.columns]
@@ -34,11 +44,20 @@ class CryptoDataDownload:
             return df
         return df
 
-    def fetch_gemini(self, base_symbol, quote_symbol, timeframe):
-        if timeframe.endswith("h"):
+    def fetch_gemini(base_symbol, quote_symbol, timeframe):
+        exchange_name = "gemini"
+        if timeframe.lower() in ['1d', 'd']:
+            exchange_name = "Gemini"
+            timeframe = 'd'
+        elif timeframe.lower() == 'h':
             timeframe = timeframe[:-1] + "hr"
-        filename = "{}_{}{}_{}.csv".format("gemini", quote_symbol, base_symbol, timeframe)
-        df = pd.read_csv(self.url + filename, skiprows=1)
+
+        filename = "{}_{}{}_{}.csv".format(exchange_name, 
+                                           base_symbol, 
+                                           quote_symbol, 
+                                           timeframe)
+        df = pd.read_csv(CryptoDataDownload.url + filename, 
+                         skiprows=1)
         df = df[::-1]
         df = df.drop(["Symbol", "Unix Timestamp"], axis=1)
         df.columns = [name.lower() for name in df.columns]
@@ -46,7 +65,18 @@ class CryptoDataDownload:
         df = df.reset_index()
         return df
 
-    def fetch(self, exchange_name, base_symbol, quote_symbol, timeframe, include_all_volumes=False):
+    def fetch(exchange_name='Coinbase', 
+              base_symbol='BTC', 
+              quote_symbol='USD', 
+              timeframe='1h', 
+              include_all_volumes=False):
+        
         if exchange_name.lower() == "gemini":
-            return self.fetch_gemini(base_symbol, quote_symbol, timeframe)
-        return self.fetch_default(exchange_name, base_symbol, quote_symbol, timeframe, include_all_volumes=False)
+            return CryptoDataDownload.fetch_gemini(base_symbol, 
+                                                   quote_symbol, 
+                                                   timeframe)
+        return CryptoDataDownload.fetch_default(exchange_name, 
+                                                base_symbol,
+                                                quote_symbol, 
+                                                timeframe, 
+                                                include_all_volumes=False)
