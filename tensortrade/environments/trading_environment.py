07caeed06a83b979bb350744d9574292c57ed17e
Kodiak <62376262+kodiakcrypto@users.noreply.github.com>
2020-06-01 02:45:50 -0700

    IDK how to access the logger messages lol

--- tensortrade/environments/trading_environment.py
+++ tensortrade/environments/trading_environment.py
@@ -260,10 +260,15 @@ class TradingEnvironment(gym.Env, TimeIndexed):
             if orders:
                 for order in orders:
                     self.logger.debug('Order:       {}'.format(order))
+                    print('Order:       {}'.format(order))
             self.logger.debug('Observation: {}'.format(obs))
+            print('Observation: {}'.format(obs))
             self.logger.debug('P/L:         {}'.format(self._portfolio.profit_loss))
+            print('P/L:         {}'.format(self._portfolio.profit_loss))
             self.logger.debug('Reward ({}): {}'.format(self.clock.step, reward))
+            print('Reward ({}): {}'.format(self.clock.step, reward))
             self.logger.debug('Performance: {}'.format(self._portfolio.performance.tail(1)))
+            print('Performance: {}'.format(self._portfolio.performance.tail(1)))
 
         self.clock.increment()
 
