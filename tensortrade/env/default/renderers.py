b878bd2073e7dfbbea7950b01f157f8a33789902
Charles Edwards <charles@capriole.io>
2020-11-04 10:37:19 +0100

    plotly tests continued

--- tensortrade/env/default/renderers.py
+++ tensortrade/env/default/renderers.py
@@ -122,7 +122,7 @@ class BaseRenderer(Renderer):
         if len(env.observer.renderer_history) > 0:
             price_history = pd.DataFrame(env.observer.renderer_history)
 
-        performance = env.action_scheme.portfolio.performance
+        performance = pd.DataFrame.from_dict(env.action_scheme.portfolio.performance, orient='index')
 
         self.render_env(
             episode=kwargs.get("episode", None),
