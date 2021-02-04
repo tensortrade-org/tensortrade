fe87bd28aedce48b3024d1793ff10b3541902270
Carlo Grisetti <carlo.grisetti@dsgroup.it>
2021-02-05 00:43:34 +0100

    Update copyright and set version to next dev0

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright 2020 The TensorTrade Authors.
+# Copyright 2021 The TensorTrade Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
