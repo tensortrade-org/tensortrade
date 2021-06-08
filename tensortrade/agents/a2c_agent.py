60f6531e27a93bf7614edbeafe7299d03f8331b1
Parsa Sadri <p_sadri_s@yahoo.com>
2021-06-08 12:33:12 +0430

    Fix only one episode training of A2C agent

--- tensortrade/agents/a2c_agent.py
+++ tensortrade/agents/a2c_agent.py
@@ -224,7 +224,6 @@ class A2CAgent(Agent):
 
                 if n_steps and steps_done >= n_steps:
                     done = True
-                    stop_training = True
 
             is_checkpoint = save_every and episode % save_every == 0
 
