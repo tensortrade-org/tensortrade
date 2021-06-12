d119a3d2323617be9dde7ebc8dbfe96a66e4cca7
Parsa Sadri <p_sadri_s@yahoo.com>
2021-06-13 01:50:05 +0430

    Update actions.py

--- tensortrade/env/default/actions.py
+++ tensortrade/env/default/actions.py
@@ -302,6 +302,132 @@ class SimpleOrders(TensorTradeActionScheme):
             order.attach(self._order_listener)
 
         return [order]
+    
+    
+class SimpleOrdersThreeType(TensorTradeActionScheme):
+    """A discrete action scheme that determines actions based on a list of
+    trading pairs, order criteria, and trade sizes.
+
+    Parameters
+    ----------
+    criteria : List[OrderCriteria]
+        A list of order criteria to select from when submitting an order.
+        (e.g. MarketOrder, LimitOrder w/ price, StopLoss, etc.)
+    trade_sizes : List[float]
+        A list of trade sizes to select from when submitting an order.
+        (e.g. '[1, 1/3]' = 100% or 33% of balance is tradable.
+        '4' = 25%, 50%, 75%, or 100% of balance is tradable.)
+    durations : List[int]
+        A list of durations to select from when submitting an order.
+    trade_type : TradeType
+        A type of trade to make.
+    order_listener : OrderListener
+        A callback class to use for listening to steps of the order process.
+    min_order_pct : float
+        The minimum value when placing an order, calculated in percent over net_worth.
+    min_order_abs : float
+        The minimum value when placing an order, calculated in absolute order value.
+    """
+
+    def __init__(self,
+                 criteria: 'Union[List[OrderCriteria], OrderCriteria]' = None,
+                 trade_sizes: 'Union[List[float], int]' = 1,
+                 durations: 'Union[List[int], int]' = None,
+                 trade_type: 'TradeType' = TradeType.MARKET,
+                 order_listener: 'OrderListener' = None,
+                 min_order_pct: float = 0.00,
+                 min_order_abs: float = 0.00) -> None:
+        super().__init__()
+        self.min_order_pct = min_order_pct
+        self.min_order_abs = min_order_abs
+        criteria = self.default('criteria', criteria)
+        self.criteria = criteria if isinstance(criteria, list) else [criteria]
+
+        trade_sizes = self.default('trade_sizes', trade_sizes)
+        if isinstance(trade_sizes, list):
+            self.trade_sizes = trade_sizes
+        else:
+            self.trade_sizes = [(x + 1) / trade_sizes for x in range(trade_sizes)]
+
+        durations = self.default('durations', durations)
+        self.durations = durations if isinstance(durations, list) else [durations]
+
+        self._trade_type = self.default('trade_type', trade_type)
+        self._order_listener = self.default('order_listener', order_listener)
+
+        self._action_space = None
+        self.actions = None
+
+    @property
+    def action_space(self) -> Space:
+        if not self._action_space:
+            self.actions = product(
+                self.criteria,
+                self.trade_sizes,
+                self.durations,
+                [TradeSide.BUY, TradeSide.SELL]
+            )
+            self.actions = list(self.actions)
+            self.actions = list(product(self.portfolio.exchange_pairs, self.actions))
+            self.actions = [None] + self.actions
+
+            self._action_space = Discrete(len(self.actions))
+        return self._action_space
+
+    def get_orders(self,
+                   action: int,
+                   portfolio: 'Portfolio') -> 'List[Order]':
+
+        if action == 0:
+            return []
+        
+#         print(action)
+
+        (ep, (criteria, proportion, duration, side)) = self.actions[action]
+        
+#         print(criteria, proportion, duration, side)
+
+        instrument = side.instrument(ep.pair)
+        wallet = portfolio.get_wallet(ep.exchange.id, instrument=instrument)
+        
+#         print(instrument)
+#         print(wallet)
+
+        balance = wallet.balance.as_float()
+        size = (balance * proportion)
+        size = min(balance, size)
+
+        quantity = (size * instrument).quantize()
+        
+#         print(balance, size, quantity)
+
+        if size < 10 ** -instrument.precision \
+                or size < self.min_order_pct * portfolio.net_worth \
+                or size < self.min_order_abs:
+            return []
+#             print(10 ** -instrument.precision)
+#             print(self.min_order_pct * portfolio.net_worth)
+#             print(self.min_order_pct)
+#             print(self.min_order_abs)
+#             print("no Order")
+            
+
+        order = Order(
+            step=self.clock.step,
+            side=side,
+            trade_type=self._trade_type,
+            exchange_pair=ep,
+            price=ep.price,
+            quantity=quantity,
+            criteria=criteria,
+            end=self.clock.step + duration if duration else None,
+            portfolio=portfolio
+        )
+
+        if self._order_listener is not None:
+            order.attach(self._order_listener)
+
+        return [order]
 
 
 class ManagedRiskOrders(TensorTradeActionScheme):
@@ -423,6 +549,7 @@ _registry = {
     'bsh': BSH,
     'simple': SimpleOrders,
     'managed-risk': ManagedRiskOrders,
+    'simple-three-type': SimpleOrdersThreeType
 }
 
 
