2ab9e0727400d43668197f3c83f8d862b99bd7bb
Tom Wieland <tom.wieland@gmail.com>
2021-08-26 08:28:08 +0200

    fix(cdd): normalize timestamps, set date by timestamp

--- tensortrade/data/cdd.py
+++ tensortrade/data/cdd.py
@@ -68,10 +68,9 @@ class CryptoDataDownload:
         df = df.drop(["symbol"], axis=1)
         df = df.rename({base_vc: new_base_vc, quote_vc: new_quote_vc, "Date": "date"}, axis=1)
 
-        if "d" in timeframe:
-            df["date"] = pd.to_datetime(df["date"])
-        elif "h" in timeframe:
-            df["date"] = pd.to_datetime(df["date"], format="%Y-%m-%d %H:%M:%S")
+        df["unix"] = df["unix"].astype(int)
+        df["unix"] = df["unix"].apply(lambda x: x * 1000 if len(str(x)) == 10 else x)
+        df["date"] = pd.to_datetime(df["unix"])
 
         df = df.set_index("date")
         df.columns = [name.lower() for name in df.columns]
