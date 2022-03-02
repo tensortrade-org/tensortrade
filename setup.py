cd2e36eae2813e22f265c839ac32f0963f1f816e
Samuel Duclos <nomfullcreatif@gmail.com>
2022-03-01 20:20:59 -0500

    This is a combination of 3 commits.
    Upgraded Ray and TensorFlow. All package management now references setup.py.
    
    Upgraded Ray and TensorFlow. All package management now references setup.py.
    
    Upgraded Ray and TensorFlow. All package management now references setup.py.

--- setup.py
+++ setup.py
@@ -30,6 +30,122 @@ with open(os.path.join(tensortrade_directory, 'tensortrade', 'version.py'), 'r')
         if line.startswith('__version__'):
             version = line[15:-2]
 
+
+install_requires = [
+    'numpy>=1.17.0',
+    'pandas>=0.25.0',
+    'gym>=0.15.7',
+    'pyyaml>=5.1.2',
+    'matplotlib>=3.1.1',
+    'plotly>=4.5.0',
+    'deprecated>=1.2.13'
+]
+
+ray_requires = [
+    'ray[default,tune,rllib,serve]==1.10.0',
+]
+
+stable_baselines_requires = [
+    'stable-baselines3[extra]',
+    'sb3-contrib'
+]
+
+tensorflow_requires = [
+    'tensorflow>=2.8.0'
+]
+
+pytorch_requires = [
+    'torch>=1.10.2'
+]
+
+ta_requires = [
+    'ta>=0.4.7'
+]
+
+indicators_requires = [
+    'quantstats',
+    'TA-Lib',
+    'pandas-ta @ git+https://github.com/twopirllc/pandas-ta.git@development',
+    ta_requires[0]
+]
+
+agents_requires = [
+    'pyarrow',
+    'wandb',
+    'tensorflow-probability>=0.15.0',
+    'opencv-python'
+]
+
+binance_requires = [
+    'python-binance',
+    'binance-futures @ git+https://github.com/Binance-docs/Binance_Futures_python.git#egg=binance-futures'
+]
+
+notebook_requires = [
+    'jupyterlab>=1.1.4',
+    'ipython>=7.12.0',
+    'ipywidgets>=7.0.0'
+]
+
+examples_requires = [
+    'stochastic>=0.6.0',
+    'ccxt==1.72.37',
+    'feature_engine',
+    'scikit-learn',
+    'optuna',
+    notebook_requires[0]
+]
+
+tests_requires = [
+    'pytest>=5.1.1',
+    ta_requires[0]
+]
+
+docs_requires = [
+    'sphinx',
+    'sphinx_rtd_theme',
+    'sphinxcontrib.apidoc',
+    'nbsphinx',
+    'nbsphinx_link',
+    'recommonmark',
+    'sphinx_markdown_tables',
+    'ipykernel'
+]
+
+optional_requires = \
+    ray_requires + \
+    stable_baselines_requires + \
+    tensorflow_requires + \
+    pytorch_requires + \
+    indicators_requires + \
+    agents_requires + \
+    binance_requires + \
+    examples_requires + \
+    tests_requires + \
+    docs_requires
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Natural Language :: English',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: Financial and Insurance Industry',
+    'Intended Audience :: Information Technology',
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Topic :: Office/Business :: Financial :: Investment',
+    'Topic :: Office/Business :: Financial',
+    'Topic :: Scientific/Engineering :: Information Analysis',
+    'Topic :: System :: Distributed Computing',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
 setup(
     name='tensortrade',
     version=version,
@@ -45,54 +161,23 @@ setup(
     ],
     license='Apache 2.0',
     python_requires='>=3.7',
-    install_requires=[
-        'numpy>=1.17.0',
-        'pandas>=0.25.0',
-        'gym>=0.15.7',
-        'pyyaml>=5.1.2',
-        'stochastic>=0.6.0',
-        'tensorflow>=2.7.0',
-        'ipython>=7.12.0',
-        'matplotlib>=3.1.1',
-        'plotly>=4.5.0',
-        'deprecated>=1.2.13'
-    ],
+    install_requires=install_requires,
     extras_require={
-        'tests': [
-            'pytest>=5.1.1',
-            'ta>=0.4.7'
-        ],
-        'docs': [
-            'sphinx',
-            'sphinx_rtd_theme',
-            'sphinxcontrib.apidoc',
-            'nbsphinx',
-            'nbsphinx_link',
-            'recommonmark',
-            'sphinx_markdown_tables',
-            'ipykernel'
-        ],
+        'ray': ray_requires,
+        'stable_baselines': stable_baselines_requires,
+        'tensorflow': tensorflow_requires,
+        'pytorch': pytorch_requires,
+        'agents': agents_requires,
+        'binance': binance_requires,
+        'ta': ta_requires,
+        'indicators': indicators_requires,
+        'notebook': notebook_requires,
+        'examples': examples_requires,
+        'tests': tests_requires,
+        'docs': docs_requires,
+        'optional': optional_requires
     },
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Natural Language :: English',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Financial and Insurance Industry',
-        'Intended Audience :: Information Technology',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Office/Business :: Financial :: Investment',
-        'Topic :: Office/Business :: Financial',
-        'Topic :: Scientific/Engineering :: Information Analysis',
-        'Topic :: System :: Distributed Computing',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
+    classifiers=classifiers,
     zip_safe=False
 )
+
